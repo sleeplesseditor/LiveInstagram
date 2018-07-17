727ec97f2d0e493dc2ba92ddcf12c863993e809e
sleeplesseditor <josh@bryson.me.uk>
2018-07-17 01:17:52 +0100

    Camera setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import { Screen } from '@shoutem/ui';
+import Camera from 'react-native-camera';
 
 export default class App extends Component {
   render() {
-    <Screen />
+    return (
+      <Screen>
+        <Camera 
+          style={{flex: 1}} 
+          ref={cam => this.camera=cam}
+          aspect={Camera.constants.Aspect.fill}
+        >
+        </Camera>
+      </Screen>
+    )
   }
 }
